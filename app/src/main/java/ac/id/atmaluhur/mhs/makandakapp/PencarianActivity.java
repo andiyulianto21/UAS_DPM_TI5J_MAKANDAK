0fb4926a2b42838835da0ababa65ee71b30f5fc9
Aprizal2543 <aprizal2543@gmail.com>
2022-01-23 17:59:33 +0700

    layout menu utama dan layout chat, Aprizal_1911500055

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/makandakapp/PencarianActivity.java
@@ -0,0 +1,16 @@
+package ac.id.atmaluhur.mhs.makandakapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ac.id.atmaluhur.mhs.makandakapp.R;
+
+public class PencarianActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pencarian);
+    }
+}
