f5ce0df7a43b586e20d0beb118e0cb97a08036b1
Andi Yulianto <andiyulianto121@gmail.com>
2022-01-24 09:00:42 +0700

    mendesain checkout activity

--- app/src/main/java/ac/id/atmaluhur/mhs/makandakapp/CheckoutActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/makandakapp/CheckoutActivity.java
@@ -2,15 +2,35 @@ package ac.id.atmaluhur.mhs.makandakapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import ac.id.atmaluhur.mhs.makandakapp.R;
 
 public class CheckoutActivity extends AppCompatActivity {
 
+    Button btnVoucher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.setTitle("Check Out");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blueButton)));
+
+        btnVoucher = findViewById(R.id.btnVoucher);
+
+        btnVoucher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CheckoutActivity.this, VoucherActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/ac/id/atmaluhur/mhs/makandakapp/MenuTokoActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/makandakapp/MenuTokoActivity.java
@@ -2,12 +2,17 @@ package ac.id.atmaluhur.mhs.makandakapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import ac.id.atmaluhur.mhs.makandakapp.R;
 
 public class MenuTokoActivity extends AppCompatActivity {
+
+    Button btnCheckout_nasgorbiasa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,17 @@ public class MenuTokoActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.setTitle("Menu");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blueButton)));
+
+        btnCheckout_nasgorbiasa = findViewById(R.id.btnCheckout_nasgorbiasa);
+
+        btnCheckout_nasgorbiasa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuTokoActivity.this, CheckoutActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
 
