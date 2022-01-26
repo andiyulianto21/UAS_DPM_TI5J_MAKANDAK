a176eb9b7f0212dad3e8c0dced737c9609f7166c
Andi Yulianto <andiyulianto121@gmail.com>
2022-01-26 14:01:10 +0700

    menambahkan intent ke beberapa activity

--- app/src/main/java/ac/id/atmaluhur/mhs/makandakapp/PencarianActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/makandakapp/PencarianActivity.java
@@ -2,15 +2,26 @@ package ac.id.atmaluhur.mhs.makandakapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import ac.id.atmaluhur.mhs.makandakapp.R;
 
 public class PencarianActivity extends AppCompatActivity {
 
+    Button btnSearch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pencarian);
+
+        btnSearch = findViewById(R.id.btnSearch);
+
+        btnSearch.setOnClickListener(view -> {
+            Intent toDetail = new Intent(PencarianActivity.this, DetailMakananActivity.class);
+            startActivity(toDetail);
+        });
     }
 }
